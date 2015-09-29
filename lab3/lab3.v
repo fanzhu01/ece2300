49b735201449618145223549b925039b88839202
ece2300 <fz89@cornell.edu>
2015-09-29 00:51:59 -0400

    aa

--- lab3/lab3.v
+++ lab3/lab3.v
@@ -15,47 +15,54 @@ module lab3(CLK, RESET, ENABLE, TIME, MIN, TENSEC, SEC, DECISEC, CENTISEC);
   assign en3 = (SEC      == 4'd9 && DECISEC  == 4'd9 && CENTISEC == 4'd9 && ENABLE) ? 1 : 0;
   assign en4 = (TENSEC   == 4'd5 && SEC      == 4'd9 && DECISEC  == 4'd9 
 					&& CENTISEC == 4'd9 && ENABLE) ? 1 : 0;
+  assign CLR1= ~(RESET || (CENTISEC == 4'd9 && ENABLE));
+  assign CLR2= ~(RESET || (CENTISEC == 4'd9 && DECISEC == 4'd9 && ENABLE));
+  assign CLR3= ~(RESET || (CENTISEC == 4'd9 && DECISEC == 4'd9 && SEC == 4'd9 && ENABLE));
+  assign CLR4= ~(RESET || (TENSEC   == 4'd5 && SEC == 4'd9 && DECISEC  == 4'd9 
+					&& CENTISEC == 4'd9 && ENABLE));
+  assign CLR5= ~(RESET || (TENSEC   == 4'd5 && SEC == 4'd9 && DECISEC  == 4'd9 
+					&& CENTISEC == 4'd9 && MIN == 4'd9 && ENABLE));
   
   // ADD YOUR CODE BELOW THIS LINE
   tcounter tcnt1 (
     .CLK(CLK),
-    .CLR(~RESET),
+    .CLR(CLR1),
     .ENP(ENABLE),
     .ENT(1'b1),
-	 .range(4'd10),
+//	 .range(4'd10),
     .Q(CENTISEC)
   );
   tcounter tcnt2 (
     .CLK(CLK),
-    .CLR(~RESET),
+    .CLR(CLR2),
     .ENP(ENABLE),
     .ENT(en1),
-	 .range(4'd10),
+	// .range(4'd10),
     .Q(DECISEC)
   );
   tcounter tcnt3 (
     .CLK(CLK),
-    .CLR(~RESET),
+    .CLR(CLR3),
     .ENP(ENABLE),
     .ENT(en2),
-	 .range(4'd10),
+//	 .range(4'd10),
     .Q(SEC)
   );
   tcounter tcnt4 (
     .CLK(CLK),
-    .CLR(~RESET),
+    .CLR(CLR4),
     .ENP(ENABLE),
     .ENT(en3),
-	 .range(4'd6),
+//	 .range(4'd6),
     .Q(TENSEC)
   );
   
   tcounter tcnt5 (
     .CLK(CLK),
-    .CLR(~RESET),
+    .CLR(CLR5),
     .ENP(ENABLE),
     .ENT(en4),
-	 .range(4'd10),
+//	 .range(4'd10),
     .Q(MIN)
   );
   // ADD YOUR CODE ABOVE THIS LINE
--- lab3/tcounter.v
+++ lab3/tcounter.v
@@ -1,10 +1,10 @@
-module tcounter(CLK, CLR, ENP, ENT, range, Q);
+module tcounter(CLK, CLR, ENP, ENT, Q);
 
 input          CLK;
 input          CLR;
 input          ENP;
 input          ENT;
-input    [3:0] range;
+//input    [3:0] range;
 
 output     [3:0]   Q;
 
@@ -14,7 +14,7 @@ wire               go;
 wire       [3:0]   OUT;
 
 
-assign reset = ( go && OUT == range) ? 1 : 0;
+//assign reset = ( go && OUT == range) ? 1 : 0;
 assign go = (ENP & ENT) ;
 always @(*) begin
 	casez({Q, go})
@@ -24,7 +24,8 @@ always @(*) begin
 		5'b0010_1: IN = 4'b0001;
 		5'b0011_1: IN = 4'b0111;
 		5'b0100_1: IN = 4'b0001;
-		5'b0101_1: begin if (range == 4'd6) IN = 4'b0101; else IN = 4'b0011; end
+		5'b0101_1: IN = 4'b0011;
+		//5'b0101_1: begin if (range == 4'd6) IN = 4'b0101; else IN = 4'b0011; end
 		5'b0110_1: IN = 4'b0001;
 		5'b0111_1: IN = 4'b1111;
 		5'b1000_1: IN = 4'b0001;
@@ -40,7 +41,7 @@ treg4bit treg(
  	.OUT(OUT)
 );
 
-assign Q = (reset) ? 0 : OUT;
+assign Q =  OUT;
 
 
 endmodule
